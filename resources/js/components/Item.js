c75f1930ba756eef69759604b88d18ada90fbbb5
AlejandroMerc <alejandromercado011@gmail.com>
2020-10-30 19:57:41 -0400

    ruta imagen

--- resources/js/components/Item.js
+++ resources/js/components/Item.js
@@ -7,7 +7,7 @@ class  Item extends React.Component{
                 <React.Fragment>
                     <div className="card Item-Card">
                         <div className="card-body">
-                            <img src={this.props.prod_img} width= "250px" height= "250px" className="float-rigth" alt="Baner 01"/>
+                            <img src={"images/"+this.props.prod_img} width= "250px" height= "250px" className="float-rigth" alt="Baner 01"/>
                             <h5>{this.props.prod_name}</h5>
                             <h6>Precio: {this.props.prod_price}$</h6>
                             <a href="www.google.com" className="btn btn-primary" role="button">Ver</a>
