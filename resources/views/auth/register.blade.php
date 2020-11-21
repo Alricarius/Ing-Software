fd67663e43a817590d853ed237d2c964609bd508
coco <4246064@gmail.com>
2020-11-21 11:00:32 -0700

    ya ta

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,19 +5,79 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">{{ __('Registro') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nombre') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="nombre" type="text" class="form-control @error('nombre') is-invalid @enderror" name="nombre" value="{{ old('nombre') }}" required autocomplete="nombre" autofocus>
 
                                 @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="apellido" class="col-md-4 col-form-label text-md-right">{{ __('Apellido') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="apellido" type="text" class="form-control @error('apellido') is-invalid @enderror" name="apellido" value="{{ old('apellido') }}" required autocomplete="apellido" autofocus>
+
+                                @error('apellido')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="Carnet" class="col-md-4 col-form-label text-md-right">{{ __('Carnet') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="carnet" type="carnet" class="form-control @error('carnet') is-invalid @enderror" name="carnet" required autocomplete="new-carnet">
+
+                                @error('carnet')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="departamento" class="col-md-4 col-form-label text-md-right">{{ __('Departamento') }}</label>
+                            <div class="col-md-6">
+                                  <select name="departamento" id = "departamento">
+                                         <option value ="Cochabamba" selcted>Cochabamba</option>
+                                         <option value ="Santa Cruz" selcted>Santa Cruz</option>
+                                         <option value ="Oruro" selcted>Oruro</option>
+                                         <option value ="La Paz" selcted>La Paz</option>
+                                         <option value ="Beni" selcted>Beni</option>
+                                         <option value ="Pando" selcted>Pando</option>
+                                         <option value ="Tarija" selcted>Tarija</option>
+                                         <option value ="Sucre" selcted>Sucre</option>
+                                         <option value ="Chuquisaca" selcted>Chuquisaca</option>
+                                         </select>
+                            </div>
+                        </div>
+
+
+                        <div class="form-group row">
+                            <label for="celular" class="col-md-4 col-form-label text-md-right">{{ __('Celular') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="celular" type="text" class="form-control @error('celular') is-invalid @enderror" name="celular" value="{{ old('celular') }}" required autocomplete="celular" autofocus>
+
+                                @error('apellido')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
@@ -52,20 +112,7 @@
                                 @enderror
                             </div>
                         </div>
-                        <div class="form-group row">
-                            <label for="Carnet" class="col-md-4 col-form-label text-md-right">{{ __('Carnet') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="carnet" type="carnet" class="form-control @error('carnet') is-invalid @enderror" name="carnet" required autocomplete="new-carnet">
-
-                                @error('carnet')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
+                        
                         <div class="form-group row">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
@@ -73,12 +120,47 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
+                                
+                        <div class="form-group row">
+                            <label for="pregunta secreta" class="col-md-4 col-form-label text-md-right">{{ __('Pregunta secreta') }}</label>
+                            <div class="col-md-6">
+                            <select name="Pregunta_Secreta" id = "Pregunta_Secreta">
+                                   <option value ="Nombre de tu primera mascota?" selcted>Nombre de tu primera mascota?</option>
+                                   <option value ="Marca de Tenis preferida?" selcted>Marca de Tenis preferida?</option>
+                                   <option value ="Deporte preferido?" selcted>Deporte preferido?</option>
+                                   </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="respuesta" class="col-md-4 col-form-label text-md-right">{{ __('Respuesta') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="respuesta" type="text" class="form-control @error('respuesta') is-invalid @enderror" name="respuesta" value="{{ old('respuesta') }}" required autocomplete="respuesta" autofocus>
+
+                                @error('apellido')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                            
+                            </div>
+                        </div>       
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
                                 </button>
+                          
+                        <div class="form-group row mb-6">
+                            <div class="col-md-0 o644ffset-md-5">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Cancelar registro') }}
+                                </button>
+ 
                             </div>
                         </div>
                     </form>
