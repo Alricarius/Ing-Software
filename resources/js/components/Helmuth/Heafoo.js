81939bdab50e0877f7cf085aee69265ea5b0783e
hlajeans <helmuthmauricioalbaquiroz@gmail.com>
2020-11-20 22:49:38 -0400

    Headers completos

--- resources/js/components/Helmuth/Heafoo.js
+++ resources/js/components/Helmuth/Heafoo.js
@@ -1,62 +1,35 @@
 import React from 'react';
 import Search from './Search'
 import './Heafoo.css'
+
 function Heafoo(props){
     return(
         <div className="heafoo">
         <header className= "header">
 		    <div className="container logo-nav-container">
 			    <a href="imagen" className="logo">MegaComputer</a>
-				    <nav className="navegacion">
-					<ul>
+				<ul className="navegacion">
 					<li><a href="#">Novedades</a></li>
-					 <li><a href="#">Asesoria</a></li>
+					<li><a href="#">Asesoria</a></li>
 					<li><a href="#">Contactenos</a></li>
-                    <Search/>
-					</ul>
-                 </nav>
+                    <li><a href="#">Cuenta</a>
+						<ul>
+						<li><a href="#">Iniciar Sesion</a></li>
+						<li><a href="#">Registrate</a></li>
+						</ul>
+					</li>	
+                </ul> 
 		    </div>
 	   </header>
 	   <main className="main">
 		<div className="container">
-			<p> Bienvenido
-			    Bienvenido
-				Bienvenido
-				Bienvenido
-				Bienvenido
-				Bienvenido
-				Bienvenido
-				Bienvenido
-				Bienvenido
-				Bienvenido
-				Bienvenido
-				Pero el elefante seguía con su ilusión, y poco a poco fue reuniendo trastos y aparatos con los que fabricar una gran cámara de fotos. Tuvo que hacerlo prácticamente todo: desde un botón que se pulsara con la trompa, hasta un objetivo del tamaño del ojo de un elefante, y finalmente un montón de hierros para poder colgarse la cámara sobre la cabeza.
-
-Así que una vez acabada, pudo hacer sus primeras fotos, pero su cámara para elefantes era tan grandota y extraña que paracecía una gran y ridícula máscara, y muchos se reían tanto al verle aparecer, que el elefante comenzó a pensar en abandonar su sueño.. Para más desgracia, parecían tener razón los que decían que no había nada que fotografiar en aquel lugar...
-
-Pero no fue así. Resultó que la pinta del elefante con su cámara era tan divertida, que nadie podía dejar de reir al verle, y usando un montón de buen humor, el elefante consiguió divertidísimas e increíbles fotos de todos los animales, siempre alegres y contentos, ¡incluso del malhumorado rino!; de esta forma se convirtió en el fotógrafo oficial de la sabana, y de todas partes acudían los animales para sacarse una sonriente foto para el pasaporte al zoo.
-
-Bienvenido
-				Bienvenido
-				Bienvenido
-				Bienvenido
-				Bienvenido
-				Bienvenido
-				Bienvenido
-				Bienvenido
-				Bienvenido
-				Bienvenido
-				Pero el elefante seguía con su ilusión, y poco a poco fue reuniendo trastos y aparatos con los que fabricar una gran cámara de fotos. Tuvo que hacerlo prácticamente todo: desde un botón que se pulsara con la trompa, hasta un objetivo del tamaño del ojo de un elefante, y finalmente un montón de hierros para poder colgarse la cámara sobre la cabeza.
-
-Así que una vez acabada, pudo hacer sus primeras fotos, pero su cámara para elefantes era tan grandota y extraña que paracecía una gran y ridícula máscara, y muchos se reían tanto al verle aparecer, que el elefante comenzó a pensar en abandonar su sueño.. Para más desgracia, parecían tener razón los que decían que no había nada que fotografiar en aquel lugar...
-
-Pero no fue así. Resultó que la pinta del elefante con su cámara era tan divertida, que nadie podía dejar de reir al verle, y usando un montón de buen humor, el elefante consiguió divertidísimas e increíbles fotos de todos los animales, siempre alegres y contentos, ¡incluso del malhumorado rino!; de esta forma se convirtió en el fotógrafo oficial de la sabana, y de todas partes acudían los animales para sacarse una sonriente foto para el pasaporte al zoo.
-
+			<p> 
+			hola
 			</p>
 		</div>
 	</main>
        <footer className="footer">
-		<p>Direccion: Calle Heroes del Chaco #1336 Zona Sarco<br/>
+		<p className="Nota">Direccion: Calle Heroes del Chaco #1336 Zona Sarco<br/>
 		Telefono: 4454654<br/>
 		Cochabamba - Bolivia
 		</p>
