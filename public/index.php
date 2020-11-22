d9146170a80b85162843e1f2ef613482e07e41ae
carlosACL <elfijocl@gmail.com>
2020-11-22 17:03:47 -0400

    index con el inicio

--- public/index.php
+++ public/index.php
@@ -1,20 +1,55 @@
-<!DOCTYPE html>
-<html lang="en" style="min-height: 100%; height: 100%;">
-    <head>
-        <meta name= "csrf-token"    content = "{{ csrf_token() }}">
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../css/app.css">
-        <title>Document</title>
-    </head>
-    <body style="height: inherit; min-height: 100%; width: 100%;">
-        <div style = "display: flex; min-height: 100%; flex-direction: column;">
-            <header id="header_comp"></header>
-            <main style="flex:1;">
-                    <div id="item"></div>
-            </main>
-            <footer id="footer_comp"></footer>
-        </div>
-    </body>
-    <script type="text/javascript" src="js/app.js"></script>
-</html>
+<?php
+
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+/*
+|--------------------------------------------------------------------------
+| Check If Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is maintenance / demo mode via the "down" command we
+| will require this file so that any prerendered template can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
+
+if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
+    require __DIR__.'/../storage/framework/maintenance.php';
+}
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = tap($kernel->handle(
+    $request = Request::capture()
+))->send();
+
+$kernel->terminate($request, $response);
