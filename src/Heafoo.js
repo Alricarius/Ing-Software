94d19cadf79a6a40a033eba6854c2e06fa41eb2b
hlajeans <helmuthmauricioalbaquiroz@gmail.com>
2020-11-27 21:55:37 -0400

    Codigo Actualizado

---
+++ src/Heafoo.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Search from './Search'
+import './Heafoo.css'
+
+function Heafoo(props){
+    return(
+        <div className="heafoo">
+        <header className= "header">
+		    <div className="container logo-nav-container">
+			    <a href="imagen" className="logo">MegaComputer</a>
+				<br/>
+				<label className="usr">Usuario</label>
+				<ul className="navegacion">
+					<li><a href="#">Novedades</a></li>
+					<li><a href="#">Asesoria</a></li>
+					<li><a href="#">Contactenos</a></li>
+                    <li><a href="#">Cuenta</a>
+						<ul>
+						<li><a href="#">Iniciar Sesion</a></li>
+						<li><a href="#">Registrate</a></li>
+						<li><a href="#">Salir</a></li>
+						<li><a href="#">Registrar Producto</a></li>
+						<li><a href="#">Agregar Administrador</a></li>
+						</ul>
+					</li>	
+                </ul> 
+		    </div>
+	   </header>
+	   <main className="main">
+		<div className="container">
+			<p> 
+			hola
+			</p>
+		</div>
+	</main>
+       <footer className="footer">
+		<p className="Nota">Direccion: Calle Heroes del Chaco #1336 Zona Sarco<br/>
+		Telefono: 4454654<br/>
+		Cochabamba - Bolivia
+		</p>
+	</footer>
+       </div>
+    );
+}
+export default Heafoo;
