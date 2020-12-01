373d9afa701ee8da81aaeadbdcfac07ed4c191d8
adrianC56 <adriancrespog3@gmail.com>
2020-11-30 22:28:54 -0400

    Arreglos de validaciones en Reg. Usuario

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="nombre" class="col-md-4 col-form-label text-md-right">{{ __('Nombre') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nombre') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="nombre" autofocus>
@@ -20,7 +20,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="apellido" class="col-md-4 col-form-label text-md-right">{{ __('Apellido') }}</label>
+                            <label for="ape_user" class="col-md-4 col-form-label text-md-right">{{ __('Apellido') }}</label>
 
                             <div class="col-md-6">
                                 <input id="ape_user" type="text" class="form-control @error('ape_user') is-invalid @enderror" name="ape_user" value="{{ old('ape_user') }}" required autocomplete="apellido" autofocus>
@@ -28,7 +28,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="carnet" class="col-md-4 col-form-label text-md-right">{{ __('Carnet') }}</label>
+                            <label for="carnet_user" class="col-md-4 col-form-label text-md-right">{{ __('Carnet') }}</label>
 
                             <div class="col-md-6">
                                 <input id="carnet_user" type="number" class="form-control @error('carnet_user') is-invalid @enderror" name="carnet_user" required autocomplete="new-carnet">
@@ -36,7 +36,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="departamento" class="col-md-4 col-form-label text-md-right">{{ __('Departamento') }}</label>
+                            <label for="dep_user" class="col-md-4 col-form-label text-md-right">{{ __('Departamento') }}</label>
                             <div class="col-md-6">
                                   <select name="dep_user" id = "dep_user">
                                          <option value ="Cochabamba" selcted>Cochabamba</option>
@@ -54,7 +54,7 @@
 
 
                         <div class="form-group row">
-                            <label for="celular" class="col-md-4 col-form-label text-md-right">{{ __('Celular') }}</label>
+                            <label for="cel_user" class="col-md-4 col-form-label text-md-right">{{ __('Celular') }}</label>
 
                             <div class="col-md-6">
                                 <input id="cel_user" type="text" class="form-control @error('cel_user') is-invalid @enderror" name="cel_user" value="{{ old('cel_user') }}" required autocomplete="celular" autofocus>
@@ -86,7 +86,7 @@
                         </div>
                                 
                         <div class="form-group row">
-                            <label for="pregunta secreta" class="col-md-4 col-form-label text-md-right">{{ __('Pregunta secreta') }}</label>
+                            <label for="pregunta" class="col-md-4 col-form-label text-md-right">{{ __('Pregunta secreta') }}</label>
                             <div class="col-md-6">
                             <select name="pregunta" id = "pregunta">
                                    <option value ="Nombre de tu primera mascota?" selcted>Nombre de tu primera mascota?</option>
@@ -131,7 +131,7 @@
     //validacion para el campo nombre
     var nombre;
     function iniciar(){
-        nombre = document.getElementById("nombre");
+        nombre = document.getElementById("name");
         nombre.addEventListener("input",validacion);
         validacion();
     }
@@ -151,7 +151,7 @@
 //validacion para el campo apellido
     var apellido;
     function iniciar1(){
-        apellido = document.getElementById("apellido");
+        apellido = document.getElementById("ape_user");
         apellido.addEventListener("input",validacion1);
         validacion1();
     }
@@ -170,7 +170,7 @@
 //validacion para el campo carnet
     var carnet;
     function iniciar2(){
-        carnet = document.getElementById("carnet");
+        carnet = document.getElementById("carnet_user");
         carnet.addEventListener("input",validacion2);
         validacion2();
     }
@@ -187,7 +187,7 @@
     //validacion para el campo celular
     var celular;
     function iniciar3(){
-        celular = document.getElementById("celular");
+        celular = document.getElementById("cel_user");
         celular.addEventListener("input",validacion3);
         validacion3();
     }
@@ -255,6 +255,20 @@ var contraseña_conf;
             respuesta.setCustomValidity("");
         }
     }
-    window.addEventListener("load", iniciar6);  
+    window.addEventListener("load", iniciar6);
+    
+    //Limpiar los campos del registro
+    var correo;
+    function limpiarReg(){
+        correo = document.getElementById("email");
+
+        nombre.value = "";
+        apellido.value = "";
+        carnet.value = "";
+        celular.value = "";
+        respuesta.value = "";
+        correo.value = "";
+    }
+    window.addEventListener("unload", limpiarReg);
 </script>
 @endsection
