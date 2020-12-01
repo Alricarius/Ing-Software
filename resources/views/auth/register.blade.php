5194abd2928aa4a374849a097de176341c691ff1
coco <4246064@gmail.com>
2020-12-01 01:48:28 -0700

    semi

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Registro') }}</div>
+                <div class="card-header offset-md-5">{{ __('Registro') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -31,7 +31,7 @@
                             <label for="carnet" class="col-md-4 col-form-label text-md-right">{{ __('Carnet') }}</label>
 
                             <div class="col-md-6">
-                                <input id="carnet_user" type="number" class="form-control @error('carnet_user') is-invalid @enderror" name="carnet_user" required autocomplete="new-carnet">
+                                <input id="carnet_user" type="number" class="form-control @error('carnet_user') is-invalid @enderror" name="carnet_user" required autocomplete="new-carnet" autofocus>
                             </div>
                         </div>
 
@@ -62,15 +62,15 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo electronico') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="text" placeholder="ejm.@gmail.com" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" >
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -78,7 +78,7 @@
                         </div>
                         
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmar contrseña') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -104,21 +104,18 @@
                             </div>
                         </div>
                             
+                        <label class="col-md-5 " ></label>
+                                <button type="submit"   >
+                                    {{ __('Registrarse') }}
+                                </button>
+ 
+
+
+
                             </div>
                         </div>       
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" id ="btmReg" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                          
-                        <div class="form-group row mb-6">
-                            <div class="col-md-0 o644ffset-md-5">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Cancelar registro') }}
-                                </button>
- 
+
                             </div>
                         </div>
                     </form>
