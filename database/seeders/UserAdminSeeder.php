da01012570f5594e4ab52d70727f6d0f1d45b376
carlosACL <elfijocl@gmail.com>
2020-12-02 02:18:41 -0400

    deployado papu

--- database/seeders/UserAdminSeeder.php
+++ database/seeders/UserAdminSeeder.php
@@ -27,6 +27,7 @@ class UserAdminSeeder extends Seeder
             'respuesta' => 'admin',
             'cel_user' => 12345678,
             'tipo_user' => 1,
+            'email_verified_at' => '2020/12/02',
         ]);
     }
 }
