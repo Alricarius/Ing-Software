f5b022251a08410e0877a9cd09e5b53aabce2310
Accelerator671 <kprojet671@gmail.com>
2020-12-02 01:11:41 -0400

    seeder del administrador

---
+++ database/seeders/UserAdminSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->truncate();
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'ape_user' => 'admin',
+            'carnet_user' => 7777777,
+            'dep_user' => 'Cochabamba',
+            'email' => 'admin@gmail.com',
+            'password' => '1234567',
+            'pregunta' => 'nombre del primer perro',
+            'respuesta' => 'admin',
+            'cel_user' => 12345678,
+            'tipo_user' => 1,
+        ]);
+    }
+}
