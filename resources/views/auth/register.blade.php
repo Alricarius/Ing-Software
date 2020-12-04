c9a0bab75d458ac98fa1ed668a487786123b326a
carlosACL <elfijocl@gmail.com>
2020-12-04 00:27:19 -0400

    tamanios de camposñññ

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
-                <div class="card-header offset-md-5">{{ __('Registro') }}</div>
+                <div class="card-header">{{ __('Registro') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -28,7 +28,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="carnet_user" class="col-md-4 col-form-label text-md-right">{{ __('Carnet') }}</label>
+                            <label for="carnet_user" class="col-md-4 col-form-label text-md-right">{{ __('Cédula de identidad') }}</label>
 
                             <div class="col-md-6">
                                 <input id="carnet_user" type="number" class="form-control @error('carnet_user') is-invalid @enderror" name="carnet_user" required autocomplete="new-carnet" autofocus >
@@ -70,7 +70,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo electronico') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="text" placeholder="ejm.@gmail.com" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" >
+                                <input id="email" type="email" placeholder="ejm.@gmail.com" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" >
                             </div>
                         </div>
 
@@ -111,7 +111,7 @@
                             
                         <label class="col-md-5 " ></label>
                                 <button type="submit" class="btn btn-primary" Style= " color: black; border: 1px solid black;">
-                                    {{ __('Registrarse') }}
+                                    {{ __('Regístrarse') }}
                                 </button>
                             </div>
                         </div>
