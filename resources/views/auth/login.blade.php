547cd597dd7594623d127c2c606a33c245f6250d
Alricarius <poncejhon321@gmail.com>
2020-12-06 23:11:43 -0400

    soluciones de bug login y contraseña

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo Electrónico') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus maxlength="50">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus maxLength= "50" >
 
                                 
                                 @error('email')
@@ -31,7 +31,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" maxLength = "15">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -83,4 +83,71 @@
         </div>
     </div>
 </div>
+<script type="text/javascript">
+//Verificacion de longitud de email    
+    var correoE;
+    let valNom;
+    function iniciar(){
+        valNom = false;
+        correoE = document.getElementById("email");
+        correoE.addEventListener("input",validacion);
+        correoE.addEventListener("keypress",impMsg);
+    }
+
+    function validacion(){
+        if(correoE.value.length == 50){
+            valNom = true;
+            //alert("si entra a el if");
+        }else{
+            //alert("si entra a else");
+            correoE.setCustomValidity("");
+            valNom = false;
+        }
+    }
+
+    function impMsg(){
+        if(valNom){
+            //alert("si entra a impmsg");
+            correoE.setCustomValidity("La dirección de correo electrónico excede los 50 caracteres.");
+            correoE.reportValidity();
+            //correoE.focus();
+            //correoE.value = "";   
+        }
+    }
+    window.addEventListener("load", iniciar);
+    
+//Verificacion de longitud de password
+    var pass1;
+    let valNom1;
+    function iniciar1(){
+        valNom1 = false;
+        pass1 = document.getElementById("password");
+        pass1.addEventListener("input",validacion1);
+        pass1.addEventListener("keypress",impMsg1);
+    }
+
+    function validacion1(){
+        if(pass1.value.length == 15){
+            valNom1 = true;
+            //alert("si entra a el if");
+        }else{
+            //alert("si entra a else");
+            pass1.setCustomValidity("");
+            valNom1 = false;
+        }
+    }
+
+    function impMsg1(){
+        if(valNom1){
+            //alert("si entra a impmsg");
+            pass1.setCustomValidity("La contraseña excede los 15 caracteres permitidos.");
+            pass1.reportValidity();
+            //correoE.focus();
+            //correoE.value = "";   
+        }
+    }
+    window.addEventListener("load", iniciar1);
+
+   }
+</script>
 @endsection
